e7fb1412f8a4d201e4ef91b58b8ec54198f23a2f
VictoriaSilva11 <victoria.silva7@estudante.ifms.edu.br>
2025-06-09 09:48:33 -0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+//import './index.css' apaguei
+import App from './App.tsx'
+import Header from './Header.tsx'
+import Body from './Body.tsx'
+import Footer from './Footer.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+    <Header />
+    <Body />
+    <Footer />
+  </StrictMode>,
+)
