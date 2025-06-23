ed6ed04307d36621ed29d238452f020b35ce085f
VictoriaSilva11 <victoria.silva7@estudante.ifms.edu.br>
2025-06-23 11:21:50 -0300

    alterações

--- src/Pagina.tsx
+++ src/Pagina.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import './Pagina.css'
 interface ProdutosState {
     id: number,
     nome: string,
@@ -7,15 +8,44 @@ interface ProdutosState {
 }
 
 function Pagina() {
+    useEffect(()=>{},[])
+    const [id,setId] = useState("")
+    const [nome,setNome] = useState("")
+    const [preco,setPreco] = useState("")
+    const [categoria,setCategoria] = useState("")
     const [produtos, setProdutos] = useState<ProdutosState[]>([
         {
-            id:1,
-            nome:"Caderno",
-            preco:20,
-            categoria:"Escolar"
+            id: 1,
+            nome: "Caderno",
+            preco: 20,
+            categoria: "Escolar"
         }
     ])
-    function TrataCadastro(){}
+    function TrataCadastro(event:React.FormEvent<HTMLFormElement>){
+        event.preventDefault();
+        //Criar um novo produto
+        const novoProduto:ProdutosState = {
+            id:parseInt(id),
+            nome:nome,
+            preco:parseFloat(preco),
+            categoria:categoria
+        }
+        //Adicionar esse novo produto no vetor/Array de produtos
+        setProdutos([...produtos,novoProduto])
+        
+    }
+    function trataId(event:React.ChangeEvent<HTMLInputElement>){
+        setId(event.target.value)
+    }
+    function trataNome(event:React.ChangeEvent<HTMLInputElement>){
+        setNome(event.target.value)
+    }
+    function trataPreco(event:React.ChangeEvent<HTMLInputElement>){
+        setPreco(event.target.value)
+    }
+    function trataCategoria(event:React.ChangeEvent<HTMLInputElement>){
+        setCategoria(event.target.value)
+    }
     return (
         <>
             <header>
@@ -53,11 +83,14 @@ function Pagina() {
                     })}
                 </div>
                 <div className="container-cadastro">
-                    <input type="text" name="id" id="id" />
-                    <input type="text" name="nome" id="nome" />
-                    <input type="text" name="preco" id="preco" />
-                    <input type="text" name="categoria" id="categoria" />
-                    <input type="submit" value="Cadastrar" onClick={TrataCadastro}/>
+                    <form onSubmit={TrataCadastro}>
+                        <input type="text" name="id" id="id" onChange={trataId} placeholder="Id"/>
+                        <input type="text" name="nome" id="nome" onChange={trataNome} placeholder="Nome"/>
+                        <input type="text" name="preco" id="preco" onChange={trataPreco} placeholder="Preço"/>
+                        <input type="text" name="categoria" id="categoria" onChange={trataCategoria} placeholder="Categoria"/>
+                        <input type="submit" value="Cadastrar"/>
+                    </form>
+
                 </div>
             </main>
             <footer></footer>
